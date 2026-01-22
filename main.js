9604f9f78a9fa9d71595f02829192bdab5d06926
neerajsingh012625-rgb <neerajsingh012625@gmail.com>
2026-01-22 18:34:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const audio = document.getElementById("bgm");
 const overlay = document.getElementById("startOverlay");
+const video = document.querySelector("video");
 
 overlay.addEventListener("click", () => {
   audio.play();
   overlay.style.display = "none";
 });
-
-const video = document.querySelector("video");
 
 const tl = gsap.timeline();
 
@@ -21,11 +20,20 @@ tl.to(".stage1",{opacity:1,duration:2})
 
   .to(".stage4",{opacity:1,duration:2})
 
-  .to(".media img:nth-child(1)",{opacity:1,duration:1},"+=1")
-  .to(".media img:nth-child(2)",{opacity:1,duration:1})
-  .to(".media img:nth-child(3)",{opacity:1,duration:1})
+  // PHOTO 1
+  .to(".m1",{opacity:1,duration:2},"+=1")
+  .to(".m1",{opacity:0,duration:2,delay:1})
 
-  .to(".media video",{opacity:1,duration:1})
+  // PHOTO 2
+  .to(".m2",{opacity:1,duration:2})
+  .to(".m2",{opacity:0,duration:2,delay:1})
+
+  // PHOTO 3
+  .to(".m3",{opacity:1,duration:2})
+  .to(".m3",{opacity:0,duration:2,delay:1})
+
+  // VIDEO
+  .to(".m4",{opacity:1,duration:2})
   .call(()=>{
     video.currentTime = 0;
     video.play();
