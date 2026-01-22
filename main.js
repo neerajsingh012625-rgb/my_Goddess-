583cec720a702fdbc05cce905d01bdef5ba51337
neerajsingh012625-rgb <neerajsingh012625@gmail.com>
2026-01-22 18:27:21 +0530

    Refactor audio and overlay event handling

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-const audio=document.getElementById("bgm");
-const overlay=document.getElementById("startOverlay");
-overlay.onclick=()=>{audio.play();overlay.style.display="none"};
+const audio = document.getElementById("bgm");
+const overlay = document.getElementById("startOverlay");
 
-const tl=gsap.timeline();
-tl.to(".stage1",{opacity:1,duration:2}).to(".stage1",{opacity:0,duration:1,delay:1})
-  .to(".stage2",{opacity:1,duration:2}).to(".stage2",{opacity:0,duration:1,delay:1})
-  .to(".stage3",{opacity:1,duration:2}).to(".stage3",{opacity:0,duration:1,delay:1})
+overlay.addEventListener("click", () => {
+  audio.play();
+  overlay.style.display = "none";
+});
+
+const video = document.querySelector("video");
+
+const tl = gsap.timeline();
+
+tl.to(".stage1",{opacity:1,duration:2})
+  .to(".stage1",{opacity:0,duration:1,delay:1})
+
+  .to(".stage2",{opacity:1,duration:2})
+  .to(".stage2",{opacity:0,duration:1,delay:1})
+
+  .to(".stage3",{opacity:1,duration:2})
+  .to(".stage3",{opacity:0,duration:1,delay:1})
+
   .to(".stage4",{opacity:1,duration:2})
+
   .to(".media img:nth-child(1)",{opacity:1,duration:1},"+=1")
   .to(".media img:nth-child(2)",{opacity:1,duration:1})
   .to(".media img:nth-child(3)",{opacity:1,duration:1})
-  .call(()=>{document.querySelector("video").play()});
+
+  .to(".media video",{opacity:1,duration:1})
+  .call(()=>{
+    video.currentTime = 0;
+    video.play();
+  });
